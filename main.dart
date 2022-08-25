e0f88ac37f411d51865289477ef61a86f17e98a3
shaker113 <shaker.baker113@gmail.com>
2022-08-25 17:59:14 +0300

    firt assignment

--- main.dart
+++ main.dart
@@ -1,4 +1,17 @@
 void main() {
   String myName = "shaker";
-  print(myName);
+  int myAge = 22;
+  Map educationInfo = ({
+    "University name ": "Jordanian university",
+    "faculty": "scince",
+    "major": "physics",
+    "graduation year": 2022
+  });
+
+  print("My name:" +
+      myName +
+      " My age: " +
+      "$myAge" +
+      " my Education info: " +
+      "$educationInfo");
 }
